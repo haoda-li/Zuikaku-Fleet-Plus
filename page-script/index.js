c3b0b21eaa004245816a3301c183e1ca1aded868
haoda <haoda.li@mail.utoronto.ca>
2019-06-20 19:56:12 -0400

    update

--- page-script/index.js
+++ page-script/index.js
@@ -1,6 +1,7 @@
 const remote = require('electron').remote;
 const wallpaper = remote.getGlobal('wallpaper');
 const setAuth = remote.require('./electron-module/authManage.js').updateWallpaperAuth;
+let image_url = "https://images7.alphacoders.com/729/thumb-1920-729345.jpg";
 
 const getBackground = () => {
   $.get("https://wall.alphacoders.com/api2.0/get.php",
@@ -12,13 +13,12 @@ const getBackground = () => {
       "width": "1920"
     },
     (data) => {
-      const image_url = data.wallpapers[Math.floor(Math.random() * 30)].url_image;
+      image_url = data.wallpapers[Math.floor(Math.random() * 30)].url_image;
+      $("#bg-link").attr("href", image_url)
       $('body').css("background-image",
                     'linear-gradient(rgba(0, 0, 0, 0.5),' +
                     ' rgba(0, 0, 0, 0.7)), ' +
                     'url(' + image_url + ')')
     }
-  ).fail(function() {
-    alert( "error" );
-  });
+  )
 }
