08d659f39ad50cf0f8120bde836fa9d59d1a9b70
haoda <haoda.li@mail.utoronto.ca>
2019-06-21 18:29:59 -0400

    update

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ let win;
 function createWindow() {
   win = new BrowserWindow({
     icon: 'icons/logo.png',
+    title: 'Zuikaku Fleet +',
     webPreferences: {
       nodeIntegration: true
     }
