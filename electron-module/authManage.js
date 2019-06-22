53b34ac6ac897b9c6cb7b50bb1d2a7af09d89e8d
haoda <haoda.li@mail.utoronto.ca>
2019-06-21 22:45:34 -0400

    update

--- electron-module/authManage.js
+++ electron-module/authManage.js
@@ -3,7 +3,7 @@ const fs =require("fs")
 const updateWallpaperAuth = (s) => {
   let info;
   try {
-    info = JSON.parse(fs.readFileSync("./auth.json"))
+    info = JSON.parse(fs.readFileSync("../settings/auth.json"))
   } catch (error) {
     info = {}
   }
@@ -13,7 +13,7 @@ const updateWallpaperAuth = (s) => {
 
 const getWallpaperAuth = () => {
   try {
-    return JSON.parse(fs.readFileSync("./auth.json")).wallpaper;
+    return JSON.parse(fs.readFileSync("./settings/auth.json")).wallpaper;
   } catch (error) {
     return null;
   }
