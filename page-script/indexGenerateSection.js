4fe356cd6cadcf130a7cf9e9b1dc1dc7029524df
yurina <lihd1003@hotmail.com>
2019-06-23 00:35:35 -0400

    init links

--- page-script/indexGenerateSection.js
+++ page-script/indexGenerateSection.js
@@ -1,18 +1,25 @@
 const generateSection = (name, image) => {
   const bgPath = "../assets/" + image + ".png"
-  const htmlContent = $(
+  const cell = $(
     '<div class="mdl-cell--middle mdl-cell mdl-cell--3-col">' +
-      '<div class="demo-card-image mdl-card mdl-shadow--2dp" style=\"background-image: url(' + bgPath + ')\">' +
-        '<div class="mdl-card__title mdl-card--expand">' +
-          '<img style="margin-left: -10px" class="section-icon" src=\"..\/assets\/' + image + '_fairy.png\" />' +
-        '</div>' +
-        '<div class="mdl-card__actions">' +
-          '<a class="section-text mdl-button mdl-js-button mdl-js-ripple-effect">' + name + '</a>' +
-        '</div>' +
-      '</div>' +
-    '</div'
+    '</div>'
   )
-  $("#sections").append(htmlContent)
+
+  const link = $('<a href="' + image +'.html"></a>')
+
+  const content = $(
+    '<div class="demo-card-image mdl-card mdl-shadow--2dp" style=\"background-image: url(' + bgPath + ')\">' +
+      '<div class="mdl-card__title mdl-card--expand">' +
+        '<img style="margin-left: -10px" class="section-icon" src=\"..\/assets\/' + image + '_fairy.png\" />' +
+      '</div>' +
+      '<div class="mdl-card__actions" style="background-color: rgba(255,255,255,0.6)">' +
+        '<span class="section-text black-text">' + name + '</span>' +
+      '</div>' +
+    '</div>'
+  )
+  link.append(content)
+  cell.append(link)
+  $("#sections").append(cell)
 }
 
 generateSection("装备图鉴", "arma")
