767791d5ba4aa4d2f1a18ef5d1e0e12765bbfd13
haoda <haoda.li@mail.utoronto.ca>
2019-07-12 16:14:50 -0400

    update

--- page-script/expedition.js
+++ page-script/expedition.js
@@ -20,3 +20,18 @@ const options = {
 
 
 const mList = new List('expd_list', options, eEdit.getExpeditions());
+
+const search = () => {
+  mList.search($("#search_input").val())
+  $("#search_input").val("")
+}
+
+$(".mdl-navigation.mdl-layout--large-screen-only").append($(
+  '<div class="mdl-textfield mdl-js-textfield">' +
+    '<input class="mdl-textfield__input" type="text" id="search_input">' +
+    '<label class="mdl-textfield__label" for="search_input">...</label>' +
+  '</div>' +
+  '<button onclick="search()" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored">' +
+    '<i class="material-icons">search</i>' +
+  '</button>')
+)
